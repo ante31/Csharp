0d2e71eeaf1a23714d82fc3497f60eb24f37ad3d
ante31 <92745972+ante31@users.noreply.github.com>
2023-06-05 18:42:27 +0200

    Add files via upload

---
+++ vjezba5/WebApplication5/Migrations/20230412165707_update2.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication5.Migrations
+{
+    /// <inheritdoc />
+    public partial class update2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Oib",
+                table: "Patients",
+                type: "nvarchar(11)",
+                maxLength: 11,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Mbo",
+                table: "Patients",
+                type: "nvarchar(18)",
+                maxLength: 18,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Oib",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(11)",
+                oldMaxLength: 11);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Mbo",
+                table: "Patients",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(18)",
+                oldMaxLength: 18);
+        }
+    }
+}
